25fbd444d91836cc0301b2fd323b73c657b3038b
thirdcake <ksktnk768@gmail.com>
2025-04-16 12:55:11 +0900

    update layout

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const siteConfig = {
     sitetitle: "Gravドキュメントの私的日本語訳",
     description: "learn.getgrav.orgを日本語に訳するものです",
     baseurl: "/learn-grav-ja",
+    hosturl: "https://thirdcake.github.io",
     bloburl: "https://github.com/thirdcake/learn-grav-ja/blob/main/src/pages",
     learnurl: "https://learn.getgrav.org",
 };
